7195db93c30dd221d4f084d6125d16d2c8a65ed4
Kuchuk Andrey <kreezag@gmail.com>
2021-04-17 10:31:07 +0400

    add copy templates and snippets from src folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
-const THEME_FOLDER = 'theme'
-const DIST_FOLDER = 'dist'
-
 
 module.exports = {
   entry: './src/scripts/index.js',
   output: {
-    path: path.resolve(__dirname, DIST_FOLDER),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: `${THEME_FOLDER}/assets`, to: path.resolve(__dirname, `${DIST_FOLDER}/assets`) },
-        { from: `${THEME_FOLDER}/config`, to: path.resolve(__dirname, `${DIST_FOLDER}/config`) },
-        { from: `${THEME_FOLDER}/layout`, to: path.resolve(__dirname, `${DIST_FOLDER}/layout`) },
-        { from: `${THEME_FOLDER}/locales`, to: path.resolve(__dirname, `${DIST_FOLDER}/locales`) },
-        { from: `${THEME_FOLDER}/sections`, to: path.resolve(__dirname, `${DIST_FOLDER}/sections`) },
-        { from: `${THEME_FOLDER}/snippets`, to: path.resolve(__dirname, `${DIST_FOLDER}/snippets`) },
-        { from: `${THEME_FOLDER}/templates`, to: path.resolve(__dirname, `${DIST_FOLDER}/templates`) },
+        'theme',
+        {
+          from: `src/pages/*/*.liquid`,
+          to: path.resolve(__dirname,`dist/templates/page.[name][ext]`)
+        },
+        {
+          from: `src/pages/common/**/*.liquid`,
+          to: path.resolve(__dirname, `dist/snippets/[name][ext]`)
+        },
       ],
     }),
   ],
