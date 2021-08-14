7e0e28396eb700b3187f1f7076d7b5433b1e2d43
Kuchuk Andrey <kreezag@gmail.com>
2021-08-14 13:54:08 +0400

    polish theme.js imports

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -8,17 +8,7 @@ import 'lazysizes/plugins/respimg/ls.respimg';
 import '../styles/theme.scss';
 
 import { focusHash, bindInPageLinks } from '@shopify/theme-a11y';
-import { cookiesEnabled } from '@shopify/theme-cart';
 
 // Common a11y fixes
 focusHash();
 bindInPageLinks();
-
-// Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
-  document.documentElement.className =
-    document.documentElement.className.replace(
-      'supports-no-cookies',
-      'supports-cookies'
-    );
-}
