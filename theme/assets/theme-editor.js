6c228d8656d5b7b85dfe3a51505df1c8ca67dce2
Andrei Kuchuk <kreezag@gmail.com>
2022-11-10 13:20:27 +0400

    [#69] add dawn theme files

---
+++ theme/assets/theme-editor.js
@@ -0,0 +1,30 @@
+document.addEventListener('shopify:block:select', function(event) {
+  const blockSelectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockSelectedIsSlide) return;
+
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  parentSlideshowComponent.pause();
+
+  setTimeout(function() {
+    parentSlideshowComponent.slider.scrollTo({
+      left: event.target.offsetLeft
+    });
+  }, 200);
+});
+
+document.addEventListener('shopify:block:deselect', function(event) {
+  const blockDeselectedIsSlide = event.target.classList.contains('slideshow__slide');
+  if (!blockDeselectedIsSlide) return;
+  const parentSlideshowComponent = event.target.closest('slideshow-component');
+  if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
+});
+
+document.addEventListener('shopify:section:load', () => {
+  const zoomOnHoverScript = document.querySelector('[id^=EnableZoomOnHover]');
+  if (!zoomOnHoverScript) return;
+  if (zoomOnHoverScript) {
+    const newScriptTag = document.createElement('script');
+    newScriptTag.src = zoomOnHoverScript.src;
+    zoomOnHoverScript.parentNode.replaceChild(newScriptTag, zoomOnHoverScript);
+  }
+});
