d638d8c94df6cc3b69dc62ec12ac6ae2131bf3ea
Anton Shabalinski <anton.shabalinski@spiralscout.com>
2023-09-25 13:36:11 +0300

    Rollup bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
-import sass from 'rollup-plugin-sass';
-import copy from 'rollup-plugin-copy'
+import { babel } from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import scss from 'rollup-plugin-scss';
+import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import delete_ from 'rollup-plugin-delete';
 import commonjs from 'rollup-plugin-commonjs';
-import { makeJsInputs, makeTemplatesInputs } from './rollup-helpers.js';
 import path from 'path';
+import { makeJsInputs, makeTemplatesInputs } from './rollup-helpers.js';
 
 const postcssPluginsSet = makeTemplatesInputs('src/templates').scss.map(
   (file) => {
@@ -15,12 +17,13 @@ const postcssPluginsSet = makeTemplatesInputs('src/templates').scss.map(
       include: file,
       extract: extractName,
       use: ['sass'],
+      sourceMap: true,
+      minimize: true,
     });
   }
 );
 
-
-// TODO: install babel, imagemin, css minifier, js minifier, remove empty files
+// TODO: imagemin, remove empty files
 export default {
   input: [
     ...makeTemplatesInputs('src/templates').js,
@@ -30,25 +33,27 @@ export default {
     dir: 'dist/assets',
   },
   plugins: [
+    delete_({
+      targets: 'dist/*',
+    }),
     copy({
       targets: [
-        {src: `theme/*`, dest: `dist`},
-        {src: 'src/layout', dest: `dist/layout`},
-        {src: 'src/assets', dest: `dist/assets`},
-        {src: 'src/templates/*/*.{liquid,json}', dest: `dist/templates`},
-        {src: 'src/customers/*/*.liquid', dest: `dist/templates/customers`},
-        {src: 'src/templates/*/*/*.liquid', dest: `dist/sections`},
-        {src: 'src/snippets/*/*.liquid', dest: `dist/snippets`},
-      ]
+        { src: `theme/*`, dest: `dist` },
+        { src: 'src/layout', dest: `dist/layout` },
+        { src: 'src/assets', dest: `dist/assets` },
+        { src: 'src/templates/*/*.{liquid,json}', dest: `dist/templates` },
+        { src: 'src/customers/*/*.liquid', dest: `dist/templates/customers` },
+        { src: 'src/templates/*/*/*.liquid', dest: `dist/sections` },
+        { src: 'src/snippets/*/*.liquid', dest: `dist/snippets` },
+      ],
     }),
-    ...postcssPluginsSet,
-    sass(),
     commonjs({
       include: 'node_modules/**',
     }),
+    babel({ babelHelpers: 'bundled' }),
     resolve(),
-    delete_({
-      targets: 'dist/*'
-    })
+    terser(),
+    ...postcssPluginsSet,
+    scss({ sourceMap: true, outputStyle: 'compressed' }),
   ],
 };
